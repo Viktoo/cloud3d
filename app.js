a7db414d5085b8dfc0e8e4835e6ba12964b5eb7c
Viktoo <vmakarskyy@gmail.com>
2019-02-20 16:26:14 -0600

    fix

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 //const port = 3000
-app.set('port', process.env.PORT || 3000);
-
+const PORT = process.env.PORT
+//app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(express.static('./model'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT)
