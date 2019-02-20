25b24a9bf4d318289e6c626b74d24f118b4ca9c1
Viktoo <vmakarskyy@gmail.com>
2019-02-20 16:18:45 -0600

    use Heroku without specifying port

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+//const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-//app.get('/model', express.static('model'))
 app.use(express.static('./model'));
-
-// app.get('/model/emoji_smile.vrx'), function (req, res) {
-// 	res.sendFile(__dirname + '/model/emoji_smile.vrx');
-// };
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(publicDir, "/index.html"));
-});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
